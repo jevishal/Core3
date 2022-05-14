fabdc62db60b5a3021790131a850030eaf755a31
Vishal <er.vishal.kmr@gmail.com>
2022-05-14 18:06:47 +0530

    MainCommitFirst

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,7 +28,7 @@ namespace Core3.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [ResponseCache(Duration = 1, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
